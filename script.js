814f332301db7c5393e3b67f386b3c5a0f28473f
thanhphatz05 <thanhphat160525@gmail.com>
2025-11-12 22:21:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Slider banner tự động
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slides img");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 4000);
